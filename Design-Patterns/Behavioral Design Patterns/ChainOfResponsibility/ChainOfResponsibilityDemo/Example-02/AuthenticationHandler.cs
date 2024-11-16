e893b1ebbdf490a9315cc8b31bd06b517b043573
sparifahmed <72200889+sparifahmed@users.noreply.github.com>
2024-11-17 02:38:33 +0600

    updated

---
+++ Design-Patterns/Behavioral Design Patterns/ChainOfResponsibility/ChainOfResponsibilityDemo/Example-02/AuthenticationHandler.cs
@@ -0,0 +1,10 @@
+﻿namespace ChainOfResponsibilityDemo.Example_02
+{
+    public class AuthenticationHandler : RequestHandlerBase
+    {
+        public override void ProcessRequest(Request request)
+        {
+            Console.WriteLine("Authentication handler processing request");
+        }
+    }
+}
---
+++ Design-Patterns/Behavioral Design Patterns/ChainOfResponsibility/ChainOfResponsibilityDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using ChainOfResponsibilityDemo.Example_01;
+
+namespace ChainOfResponsibilityDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Example_01_Demo();
+            Console.ReadKey();
+        }
+
+        static void Example_01_Demo() 
+        {
+            // Create the support handlers
+            var level3SupportHandler = new Level3SupportHandler();
+            var level2SupportHandler = new Level2SupportHandler();
+            var level1SupportHandler = new Level1SupportHandler();
+
+            // Set the chain of responsibility
+            level1SupportHandler.SetNextHandler(level2SupportHandler);
+            level2SupportHandler.SetNextHandler(level3SupportHandler);
+
+            // Create tickets
+            var ticket1 = new Ticket { Severity = Severity.Low };
+            var ticket2 = new Ticket { Severity = Severity.Medium };
+            var ticket3 = new Ticket { Severity = Severity.High };
+
+            // Process the tickets
+            level1SupportHandler.HandleTicket(ticket1);
+            level1SupportHandler.HandleTicket(ticket2);
+            level1SupportHandler.HandleTicket(ticket3);
+        }
+    }
+}
