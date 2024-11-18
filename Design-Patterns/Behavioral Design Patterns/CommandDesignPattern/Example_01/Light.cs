084480aae020ac162d057af6f34a9a23a5f3a522
sparifahmed <72200889+sparifahmed@users.noreply.github.com>
2024-11-18 10:05:45 +0600

    updated

---
+++ Design-Patterns/Behavioral Design Patterns/CommandDesignPattern/Example_01/Light.cs
@@ -0,0 +1,14 @@
+﻿namespace CommandDesignPattern.Example_01
+{
+    public class Light
+    {
+        public void On()
+        {
+            Console.WriteLine("Light is ON");
+        }
+        public void Off()
+        {
+            Console.WriteLine("Light is OFF");
+        }
+    }
+}
---
+++ Design-Patterns/Behavioral Design Patterns/CommandDesignPattern/Example_01/LightOnCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace CommandDesignPattern.Example_01
+{
+    public class LightOnCommand : ICommand
+    {
+        private Light _light;
+        public LightOnCommand(Light light) => _light = light;
+        public void Execute()
+        {
+            _light.On();
+        }
+    }
+}
