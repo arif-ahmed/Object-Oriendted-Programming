4b272e749f281c70327912a60c6cc7bc9f365a31
sparifahmed <72200889+sparifahmed@users.noreply.github.com>
2024-11-18 12:51:03 +0600

    updated

--- Design-Patterns/Behavioral Design Patterns/Observer/Program.cs
+++ Design-Patterns/Behavioral Design Patterns/Observer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Observer.CriminalSurveillanceSystem;
+using Observer.Example_01;
 using System;
 
 namespace Observer
@@ -6,6 +7,12 @@ namespace Observer
     class Program
     {
         static void Main(string[] args)
+        {
+            Example_01();
+            Console.ReadKey();
+        }
+
+        static void CriminalSurveillanceSystem()
         {
             Inspector inspector1 = new Inspector("Greg Lestrade");
             Inspector inspector2 = new Inspector("Sherlock Holmes");
@@ -18,9 +25,20 @@ namespace Observer
             headquarters.SendMessage(new Message("Catch Moriarty!"));
 
             headquarters.EndTransmission();
+        }
 
-
-            Console.ReadKey();
+        static void Example_01()
+        {
+            // Create IBM stock and attach investors
+            var ibm = new IBM(120.00);
+            // Attach 'listeners', i.e. Investors
+            ibm.Attach(new Investor { Name = "Sorros" });
+            ibm.Attach(new Investor { Name = "Berkshire" });
+            // Fluctuating prices will notify listening investors
+            ibm.Price = 120.10;
+            ibm.Price = 121.00;
+            ibm.Price = 120.50;
+            ibm.Price = 120.75;
         }
     }
 }
