ae4f547dc0d421f482d2acc0a638fc25a840d111
sparifahmed <31665411+arif-ahmed@users.noreply.github.com>
2024-12-13 17:50:24 +0600

    updated

---
+++ Low-Level-Design/ParkingLotManagement/ParkingLot.cs
@@ -0,0 +1,6 @@
+﻿namespace ParkingLotManagement
+{
+    public class ParkingLot
+    {
+    }
+}
---
+++ Low-Level-Design/ParkingLotManagement/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ParkingLotManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
